8b025c561836dc66ef7f6b319d1f15f31461706d
parul19012006 <144649846+parul19012006@users.noreply.github.com>
2026-07-22 20:44:31 +0530

    Update app.py

--- QR_Management/app.py
+++ QR_Management/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly as px
 from Scan_QR import scan
 
 FILE = "QR_Asset_Dataset.xlsx"  # FIXED: matches the actual generated filename
